0199dcacf66389aa2a1371b58885f6e2da9ed88a
yhojan-yauli <97636147+yhojan-yauli@users.noreply.github.com>
2026-01-09 18:37:04 -0500

    Creacion del proyecto final

---
+++ src/main/java/com/utp/ferreteria_construplaza/Ferreteria_ConstruPlaza.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.utp.ferreteria_construplaza;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Ferreteria_ConstruPlaza {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
